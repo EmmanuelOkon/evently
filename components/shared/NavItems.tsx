f7d8182c80e3839a45c6b0f7a5edd6674d3f5389
Emmanuel Okon <emmaudo27@gmail.com>
2024-02-13 13:31:19 +0100

    set up desktop and mobile nav, set up custom route for sign in and sign up pages

---
+++ components/shared/NavItems.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavItems = () => {
+  
+
+  return (
+    <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
+      NavItems
+    </ul>
+  );
+};
+
+export default NavItems;
